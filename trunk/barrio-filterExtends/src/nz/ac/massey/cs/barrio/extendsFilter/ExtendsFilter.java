ac5530403ba71740ac71b661b8d1edb29ab187f9
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-02-19 01:27:52 +0000

    Initial import.

---
+++ trunk/barrio-filterExtends/src/nz/ac/massey/cs/barrio/extendsFilter/ExtendsFilter.java
@@ -0,0 +1,19 @@
+package nz.ac.massey.cs.barrio.extendsFilter;
+
+import edu.uci.ics.jung.graph.Edge;
+import nz.ac.massey.cs.barrio.filters.EdgeFilter;
+
+public class ExtendsFilter extends EdgeFilter {
+
+	public boolean acceptEdge(Edge e) {
+		String type = e.getUserDatum("relationship.type").toString();
+		if (type.equals("extends")) return false;
+		return true;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Extends";
+	}
+
+}
