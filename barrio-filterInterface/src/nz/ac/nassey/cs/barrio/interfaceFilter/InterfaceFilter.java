409ca980f5995a1c37f84e63625626fc539af1b3
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-02-19 01:33:17 +0000

    Initial import.

---
+++ barrio-filterInterface/src/nz/ac/nassey/cs/barrio/interfaceFilter/InterfaceFilter.java
@@ -0,0 +1,19 @@
+package nz.ac.nassey.cs.barrio.interfaceFilter;
+
+import edu.uci.ics.jung.graph.Vertex;
+import nz.ac.massey.cs.barrio.filters.NodeFilter;
+
+public class InterfaceFilter extends NodeFilter {
+
+	public boolean acceptVertex(Vertex v) {
+		String isInterface = v.getUserDatum("class.isInterface").toString();
+		if (isInterface.equals("true")) return false;
+		return true;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Interfaces";
+	}
+
+}
