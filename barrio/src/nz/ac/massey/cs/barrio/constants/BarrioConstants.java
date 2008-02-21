2c6e3718f8dafb2537a50556a7b23d7daec30787
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-02-21 02:54:43 +0000

    graphics still the same

--- barrio/src/nz/ac/massey/cs/barrio/constants/BarrioConstants.java
+++ barrio/src/nz/ac/massey/cs/barrio/constants/BarrioConstants.java
@@ -2,9 +2,9 @@ package nz.ac.massey.cs.barrio.constants;
 
 public class BarrioConstants {
 
-	public static String GRAPHML_FOLDER = "c:/dcaPlugin";
-	public static String JUNG_GRAPH_FILE = "c:/dcaPlugin/jGraph.xml";
-	public static String PREFUSE_GRAPH_FILE = "c:/dcaPlugin/pGraph.xml";
-	public static String TEST_GRAPH_FILE = "c:/dcaPlugin/testGraph.xml";
+	public static String GRAPHML_FOLDER = "barrioPlugin";
+	public static String JUNG_GRAPH_FILE = GRAPHML_FOLDER+"/jGraph.xml";
+	public static String PREFUSE_GRAPH_FILE = GRAPHML_FOLDER+"/pGraph.xml";
+	public static String TEST_GRAPH_FILE = GRAPHML_FOLDER+"/testGraph.xml";
 
 }
