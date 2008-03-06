f758ea530e9b0e1be2f62663003f7b835c82fdd7
jens.dietrich <jens.dietrich@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-06 10:13:59 +0000

    Initial import.

---
+++ barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/inheritance/Class1.java
@@ -0,0 +1,8 @@
+package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.inheritance;
+
+import java.io.Serializable;
+
+@test.ExpectImplements("java.io.Serializable")
+public class Class1 implements Serializable {
+
+}
---
+++ barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/inheritance/Class3.java
@@ -0,0 +1,6 @@
+package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.inheritance;
+
+@test.ExpectImplements("java.lang.Cloneable,java.io.Serializable")
+public class Class3 implements java.io.Serializable, Cloneable {
+
+}
