c736dff4e81cf8585ab3a0da3f1e88d625e1afdc
jens.dietrich <jens.dietrich@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-07 09:01:55 +0000

    more test cases added

---
+++ barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/params/Class1.java
@@ -0,0 +1,11 @@
+package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.params;
+
+import java.util.*;
+
+@test.ExpectUses("java.util.Date,java.util.Calendar,java.util.List")
+public class Class1  {
+
+	public Date m(Calendar c,List l) {
+		return null;		
+	}
+}
