61807c5528c3be7e0b00381d5c342b8b59e43bed
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-10 22:58:30 +0000

    changed classname schema from "className" to "packageName.className"

--- trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Class1.java
+++ trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Class1.java
@@ -1,16 +1,17 @@
 package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava;
 
-@test.ExpectUses("Class2, Class3")
+@test.ExpectUses("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Class2, nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Class3")
+@test.ExpectPublic()
 public class Class1 {
 	
-	@test.ExpectUses("Class1")
-	@test.ExpectPrivate("Class2")
+	@test.ExpectUses("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Class1")
+	@test.ExpectPrivate()
 	private class Class2{
 		
 	}
 	
-	@test.ExpectUses("Class1")
-	@test.ExpectPublic("Class3")
+	@test.ExpectUses("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Class1")
+	@test.ExpectPublic()
 	public class Class3{
 		
 	}
--- trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Interface2.java
+++ trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Interface2.java
@@ -1,6 +1,6 @@
 package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava;
 
-@test.ExpectExtends("Interface1")
+@test.ExpectExtends("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Interface1")
 public interface Interface2 extends Interface1 {
 
 }
