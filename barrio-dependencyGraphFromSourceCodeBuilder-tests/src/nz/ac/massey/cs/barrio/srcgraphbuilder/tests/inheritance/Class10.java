87cbd06d6f5da911095d548ffdf7d4d37b7753ec
jens.dietrich <jens.dietrich@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-10 08:55:41 +0000

    more test cases added

---
+++ barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/inheritance/Class10.java
@@ -0,0 +1,7 @@
+package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.inheritance;
+
+import java.io.Serializable;
+
+@test.ExpectImplements("java.io.Serializable")
+public enum Class10 implements Serializable {
+}
