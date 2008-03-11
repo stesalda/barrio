cb9e0ff119a11056619116a1feebb82d5dbc153f
jens.dietrich <jens.dietrich@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-11 08:23:32 +0000

    more test cases added

--- barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/inheritance/Enum1.java
+++ barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/inheritance/Enum1.java
@@ -3,5 +3,5 @@ package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.inheritance;
 import java.io.Serializable;
 
 @test.ExpectImplements("java.io.Serializable")
-public enum Class10 implements Serializable {
+public enum Enum1 implements Serializable {
 }
