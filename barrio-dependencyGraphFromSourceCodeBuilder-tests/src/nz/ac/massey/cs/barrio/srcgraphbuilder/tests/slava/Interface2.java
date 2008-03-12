6bc195f8a85b701dba115acf39d62b8287a49ced
jens.dietrich <jens.dietrich@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-12 08:27:41 +0000

    removed slava tests - they are all covered by other tests

--- barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Interface2.java
+++
@@ -1,7 +0,0 @@
-package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava;
-
-@test.ExpectExtends("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Interface1")
-@test.ExpectModifier("public,abstract")
-public interface Interface2 extends Interface1 {
-
-}
