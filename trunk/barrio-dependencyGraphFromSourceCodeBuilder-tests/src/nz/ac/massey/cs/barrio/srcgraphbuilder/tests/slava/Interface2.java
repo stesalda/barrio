efb6a54f061c19a18cceb46641180c27481f6554
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-12 00:01:24 +0000

    more tests

--- trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Interface2.java
+++ trunk/barrio-dependencyGraphFromSourceCodeBuilder-tests/src/nz/ac/massey/cs/barrio/srcgraphbuilder/tests/slava/Interface2.java
@@ -1,6 +1,7 @@
 package nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava;
 
 @test.ExpectExtends("nz.ac.massey.cs.barrio.srcgraphbuilder.tests.slava.Interface1")
+@test.ExpectAbstract()
 public interface Interface2 extends Interface1 {
 
 }
