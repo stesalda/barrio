d2a992dbb5134769d1a0b13a254a27df716d3676
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-03-13 02:52:58 +0000

    trying to implement job for clustering, but not working yet

--- barrio/src/nz/ac/massey/cs/barrio/inputReader/KnownInputReader.java
+++ barrio/src/nz/ac/massey/cs/barrio/inputReader/KnownInputReader.java
@@ -6,11 +6,11 @@ import nz.ac.massey.cs.barrio.extensionFinder.ExtensionFinderUtil;
 
 public class KnownInputReader {
 
-private static List<InputReader> readers = null;
+private static List<InputReaderJob> readers = null;
 	
-	public static List<InputReader> all() {
+	public static List<InputReaderJob> all() {
 		if (readers==null) {
-			 ExtensionFinderUtil<InputReader> extensionFinder = new ExtensionFinderUtil<InputReader>();
+			 ExtensionFinderUtil<InputReaderJob> extensionFinder = new ExtensionFinderUtil<InputReaderJob>();
 			 readers = extensionFinder.findExtensionObjects("nz.ac.massey.cs.barrio.inputReader", "inputReaderClass");
 		}
 		return readers;
