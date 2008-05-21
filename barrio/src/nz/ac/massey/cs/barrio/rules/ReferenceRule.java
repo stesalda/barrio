b37874a654ac45fd6f21e8a1bee8ed04a5fa3f5b
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-05-21 10:37:28 +0000

    modifying gui to store rules.

--- barrio/src/nz/ac/massey/cs/barrio/rules/ReferenceRule.java
+++ barrio/src/nz/ac/massey/cs/barrio/rules/ReferenceRule.java
@@ -1,58 +1,46 @@
 package nz.ac.massey.cs.barrio.rules;
 
-public class ReferenceRule implements Rule{
+import java.util.List;
+
+public class ReferenceRule{
 	
-	private String conditionValue;
+	private List<RuleCondition> ruleConditions;
 	private String result;
-	private boolean not;
-	
-	public String getReference() {
-		return conditionValue;
+
+	public List<RuleCondition> getConditionValue() {
+		return ruleConditions;
 	}
-	public void setReference(String reference) {
-		this.conditionValue = reference;
-	}
+
 	public String getResult() {
 		return result;
 	}
+
+	public void setConditionValues(List<RuleCondition> conditionValues) {
+		this.ruleConditions = conditionValues;
+	}
+
 	public void setResult(String result) {
 		this.result = result;
-	}
-	public boolean isNegated() {
-		return not;
-	}
-	public void setNegation(boolean not) {
-		this.not = not;
 	}
 	
 	@Override
 	public String toString() {
+		boolean isFirst = true;
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("IF");
-		if(not) buffer.append(" DOES NOT reference \"");
-		else buffer.append(" references \"");
-		buffer.append(conditionValue);
-		buffer.append("\" THEN it is \"");
+		for(RuleCondition condition:ruleConditions)
+		{
+			if(!isFirst) buffer.append(" AND");
+			if(condition.isNegated()) buffer.append(" DOES NOT reference \"");
+			else buffer.append(" references \"");
+			buffer.append(condition.getReference());
+			buffer.append("\"");
+			isFirst = false;
+		}
+		
+		buffer.append(" THEN it is \"");
 		buffer.append(result);
 		buffer.append("\"");
 		return buffer.toString();
 	}
-	
-	public String getCondition() {
-		return "references";
-	}
-	public String getConditionValue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public String getResultStatement() {
-		return "it is";
-	}
-	public void setConditionValue(String conditionValue) {
-		this.conditionValue = conditionValue;
-		
-	}
-	
-	
-
 }
