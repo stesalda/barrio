647771f7b198075c4e9799cc0e4843906049b6e7
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-06-02 06:06:32 +0000

    upgrade of preference ui

--- barrio/src/nz/ac/massey/cs/barrio/preferences/RuleStorage.java
+++ barrio/src/nz/ac/massey/cs/barrio/preferences/RuleStorage.java
@@ -4,9 +4,12 @@ import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,22 +21,29 @@ public class RuleStorage {
 	
 	public RuleStorage(String filename) {
 		if(filename!=null && filename.length()>0) this.filename = filename;
-		else filename = "rules.xml";
+		else this.filename = "rules.xml";
 	}
 
 	public void store(List<ReferenceRule> rules)
 	{
 		if(rules==null) return;
 		try {
-			XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
+			File file = new File(filename);
+			System.out.println("[RuleStorage]: created file = "+file.createNewFile());
+			System.out.println("[RuleStorage]: save to: "+file.getAbsolutePath());
+			XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(file)));
 			for(ReferenceRule rule:rules)
 			{
 				encoder.writeObject(rule);
 			}
 			encoder.close();
+			System.out.println("[RuleStorage]: saved to: "+filename);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
@@ -43,6 +53,14 @@ public class RuleStorage {
 		XMLDecoder decoder = null;
 		boolean isNext = true;
 		try {
+			File file = new File(filename);
+			if(file.createNewFile())
+			{
+				OutputStream stream = new FileOutputStream(file);
+				stream.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>".getBytes("UTF-8"));
+				stream.write("<java version=\"1.6.0_06\" class=\"java.beans.XMLDecoder\">".getBytes("UTF-8"));
+				stream.write("</java>".getBytes("UTF-8"));
+			}
 			decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
 			
 			while(isNext)
@@ -54,6 +72,9 @@ public class RuleStorage {
 			e.printStackTrace();
 		} catch (ArrayIndexOutOfBoundsException e){
 			isNext = false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}finally{
 			decoder.close();
 		}
