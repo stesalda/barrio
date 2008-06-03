e5c1e2d0f9c5ed170cf0849910be28e48ba4416f
slava.nz <slava.nz@44200118-6146-0410-afbd-f30858b1f6e7>
2008-06-03 00:36:26 +0000

    new verison .29 - classifying simce to be ok

--- barrio-VertexClassifier/src/nz/ac/massey/cs/barrio/vertexclassifier/VertexClassifier.java
+++ barrio-VertexClassifier/src/nz/ac/massey/cs/barrio/vertexclassifier/VertexClassifier.java
@@ -7,75 +7,62 @@ import java.util.StringTokenizer;
 import edu.uci.ics.jung.graph.Vertex;
 import nz.ac.massey.cs.barrio.classifier.Classifier;
 import nz.ac.massey.cs.barrio.rules.ReferenceRule;
+import nz.ac.massey.cs.barrio.rules.RuleCondition;
 
 public class VertexClassifier implements Classifier{
 
 	public void classify(Vertex v, List<ReferenceRule> rules) 
 	{
-		List<String> references = getReferences(v.getUserDatum("reference").toString());
+		List<String> references = new ArrayList<String>();
+		if(v.getUserDatum("reference")!=null) references = getReferences(v.getUserDatum("reference").toString());
+		System.out.println("[VertexClassifier]: references = " + references);
 		List<String> classifications = new ArrayList<String>();
 		
 		for(ReferenceRule rule:rules)
 		{
-//			//boolean negate1 = rule.getCondition1().isNegated();
-//			//String cond1 = rule.getCondition1().getReference();
-//			
-//			if(rule.getCondition2()==null)
-//			{
-//				if(isSattisfying(negate1, cond1, references, classifications))
-//					classifications.add(rule.getResult());
-//			}
-//			else
-//			{//
-//				boolean negate2 = rule.getCondition2().isNegated();
-//				String cond2 = rule.getCondition2().getReference();
-//				if(isSattisfying(negate1, cond1, references, classifications) && isSattisfying(negate2, cond2, references, classifications))
-//					classifications.add(rule.getResult());
-//			}
+			if(isSattisfyingRule(references, rule)) classifications.add(rule.getResult());
 		}
-		
-		StringBuffer buffer = new StringBuffer();
-		for(String cl:classifications)
+		System.out.println("[VertexClassifier]: classifications = " + classifications);
+	}
+	
+	
+	private boolean isSattisfyingRule(List<String> references, ReferenceRule rule) 
+	{
+		for(RuleCondition condition:rule.getConditions())
 		{
-			buffer.append(cl);
-			buffer.append('|');
+			if(!isSattisfyingCondition(references, condition)) return false;
 		}
-	}
-	
-	
-	private boolean isSattisfying(boolean negate1, String cond1, List<String> references, List<String> classifications) 
-	{
-		if(!negate1 && (containsReference(cond1, references) || containsReference(cond1, classifications))) return true;
-		if(negate1 && !(containsReference(cond1, references) || containsReference(cond1, classifications))) return true;
-		return false;
+		return true;
 	}
 	
 	
 	
-	private boolean containsReference(String cond, List<String> references)
-	{
-		String condition = null;
-		if(cond.endsWith("*")) condition = cond.substring(0, cond.lastIndexOf('.'));
-		else condition = cond;
+	private boolean isSattisfyingCondition(List<String> references, RuleCondition condition) {
+		String value = condition.getValue();
+		if(value.endsWith("*")) value = value.substring(0, value.lastIndexOf('*'));
+		System.out.println("[VertexClassifier]: cond value = "+value);
 		
+		boolean contains = false;
 		for(String ref:references)
 		{
-			if(ref.startsWith(condition)) return true;
+			if(ref.startsWith(value)) contains = true;
 		}
-		return false;
+		
+		if(condition.isNegated()) return !contains;
+		return contains;
 	}
-
+	
 	
 	private List<String> getReferences(String str)
-	{
-		StringTokenizer st = new StringTokenizer(str, "|");
-		List<String> references = new ArrayList<String>();
-		while(st.hasMoreElements())
-		{
-			Object obj = st.nextElement();
-			references.add((String) obj);
-		}
-		return references;
-	}
+    {
+            StringTokenizer st = new StringTokenizer(str, "|");
+            List<String> references = new ArrayList<String>();
+            while(st.hasMoreElements())
+            {
+                    Object obj = st.nextElement();
+                    references.add((String) obj);
+            }
+            return references;
+    }
 
 }
